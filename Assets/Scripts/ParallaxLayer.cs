b43acd422a540d26dc843ced6226bd4ca535ee5d
jpparein <pareinjeanphilippe@outlook.fr>
2026-03-25 17:40:26 +0100

    Session 7

---
+++ Assets/Scripts/ParallaxLayer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+    private Transform cam;
+    [SerializeField] private float factor = 0.5f;
+    private float startX;
+   
+    void Start()
+    {
+        cam = Camera.main.transform;
+        startX = transform.position.x;
+    }
+   
+    void LateUpdate()
+    {
+        float camX = cam.position.x;
+        transform.position = new Vector3(
+            startX + camX * factor,
+            transform.position.y,
+            transform.position.z
+        );
+    }
+}
