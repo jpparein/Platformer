fe2211ae8f8ceb179148450a3f29c422ccb500f5
jpparein <pareinjeanphilippe@outlook.fr>
2026-04-30 10:27:04 +0200

    session 22

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class Options : MonoBehaviour
@@ -7,25 +8,51 @@ public class Options : MonoBehaviour
     [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
+    [SerializeField] private bool useEscapePause;
 
     private Animator animator;
+    private bool isOpen;
+    private AudioSource audioSource;
+
     private void Start()
     {
         animator = GetComponentInChildren<Animator>();
         musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
         sfxSlider.value = PlayerPrefs.GetFloat("SfxVolume", 1f);
+        audioSource = GetComponentInChildren<AudioSource>();
 
         SetMusicVolume(musicSlider.value);
         SetSfxVolume(sfxSlider.value);
     }
 
+    private void Update()
+    {
+        if (!useEscapePause) return;
+
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            if (isOpen)
+                HideOptions();
+            else
+                ShowOptions();
+
+            audioSource.Play();
+        }
+    }
+
     public void ShowOptions()
     {
+        isOpen = true;
+        if (useEscapePause)
+            Time.timeScale = 0f;
         animator.SetTrigger("Show");
     }
 
     public void HideOptions()
     {
+        isOpen = false;
+        if (useEscapePause)
+            Time.timeScale = 1f;
         animator.SetTrigger("Hide");
     }
 
